9a1b07cc04122b6d78b09b1813641026741beb81
LuisAceves <luis_aceves@msn.com>
2021-01-26 23:33:15 -0600

    prueba de guardado

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
-var edad = 17;
+/*
+var edad = 21;
 var saludo;
+
 
 if (edad >= 18) {
     saludo =  "mayor De Edad"; 
@@ -7,3 +9,72 @@ if (edad >= 18) {
     saludo = "pelas eres un niño";
 }
  console.log(saludo)
+ */
+
+ /*
+let "autor"
+
+ var libro = 2;
+var text;
+
+switch (libro) {
+    case 0:
+        text = "padrino";
+        break;
+    case 1:
+        text = "condorito";
+        break;
+    case 2:
+        text = "vaquero";
+        break;
+    case 3:
+        text = "tvnotas";
+        break;
+        default:
+            text = "Error de libro"
+}
+
+console.log(text);
+
+*/
+/*
+for(var i=1; i <= 10; i++) {
+    console.log('i vale', i);
+}
+*/
+
+
+
+
+
+
+/*
+
+for(var i=200; i >= 55; i--) {
+    i(i%3==0){
+    console.log('i vale', i);
+    } else(
+        continue
+    )
+}
+*/
+
+/*
+let valor = 100;
+
+while (valor > 10) {
+    valor = valor / 2;
+    console.log('valor', valor);
+}
+
+do {
+
+} while (valor > 10);
+*/
+
+let valor = 100;
+
+while (valor > 10) {
+    valor = valor / 2;
+    console.log('valor', valor);
+}
