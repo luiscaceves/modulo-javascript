cc8741e6ebf72c019eed05eb7ba7041724ed6b42
LuisAceves <luis_aceves@msn.com>
2021-02-08 09:02:47 -0600

    Primer intento de ejercicio 1

--- main.js
+++ main.js
@@ -1,44 +1,74 @@
-// Inicio de Ejercicio 4 de Febrero 21
+// Inicio de Ejercicio 1 clase 4 02/Febrero/21
+
+
+// let lista = [];
+
+// Agregar();
+
+//     lista.forEach((element) => {
+//         let nombre = element.nombre;
+//         let edad = element.edad;
+//         let grado = element.grado;
+//         let grupo = element.grupo;
+//         console.log(`Nombre: ${nombre}. Edad: ${edad}. Grado: ${grado}. Grupo: ${grupo}.`);
+//     })
+
+// class Persona {
+//     constructor(nombre, edad){
+//         this.nombre = nombre;
+//         this.edad = edad;
+//     }
+// }
+
+// class Alumno extends Persona {
+//     constructor(nombre, edad, grado, grupo){
+//         super(nombre, edad);
+//         this.grado = grado;
+//         this.grupo = grupo;
+//     }
+// }
+
+// function Agregar(){
+//     let respuesta = 'Y';
+//     while(respuesta=='Y'){
+//         let nombre = prompt('Nombre:');
+//         let edad = prompt('Edad:');
+//         let grado = prompt('Grado:');
+//         let grupo = prompt('Grupo:');
+//         let nuevo = new Alumno(nombre, edad, grado, grupo);
+//         lista.push(nuevo);
+//         respuesta = prompt('Deseas agregar un nuevo Alumno? Y/N');
+//     }
+// }
 
 
 let lista = [];
 
-Agregar();
+let seleccion = (prompt('A agregar, B borrar, C buscar, D mostrar lista'));
 
-    lista.forEach((element) => {
-        let nombre = element.nombre;
-        let edad = element.edad;
-        let grado = element.grado;
-        let grupo = element.grupo;
-        console.log(`Nombre: ${nombre}. Edad: ${edad}. Grado: ${grado}. Grupo: ${grupo}.`);
-    })
+    switch (seleccion){
+        case A:
+            lista.push(prompt('Añadir un elemento a la lista?'));
+            break;
+        case B:
+            lista.slice(lista.lastIndexOf(prompt('Eliminar un elemento?')),A)
+            break;
+        case C:
+            if(lista.indexOf(prompt('Saber si un elemento existe?'))){
+                console.log('NO TENGO LO QUE BUSCAS')
+            }
+            else{
+                console.log('SI TENGO LO QUE BUSCAS')
+            }
+            break;
+        case D:
+            console.log(lista);
+            break;
+        default:
+        break;
 
-class Persona {
-    constructor(nombre, edad){
-        this.nombre = nombre;
-        this.edad = edad;
+
     }
-}
 
-class Alumno extends Persona {
-    constructor(nombre, edad, grado, grupo){
-        super(nombre, edad);
-        this.grado = grado;
-        this.grupo = grupo;
-    }
-}
-
-function Agregar(){
-    let respuesta = 'Y';
-    while(respuesta=='Y'){
-        let nombre = prompt('Nombre:');
-        let edad = prompt('Edad:');
-        let grado = prompt('Grado:');
-        let grupo = prompt('Grupo:');
-        let nuevo = new Alumno(nombre, edad, grado, grupo);
-        lista.push(nuevo);
-        respuesta = prompt('Deseas agregar un nuevo Alumno? Y/N');
-    }
-}
 
 
---
+++ sesion04/sesion4.js
@@ -0,0 +1,60 @@
+/* let lista= [0,3,46,7,'antonio',[0,2,2],'ul',4] */
+
+// console.log(lista[9]);
+
+// lista.length
+// lista['length'];
+
+// lista.push()
+// lista[];
+
+// let nueva = lista.splice(9,10.'Nuevo','otro');
+
+// console.log(lista);
+ 
+// var a = 5;
+// var b = 10;
+// var c= 15;
+
+
+
+// console.log((3 + b) * c / a * 2);
+
+
+
+// console.log(((a + b * c) / 5) * 2);
+
+
+
+// console.log(b / a + 2 * c);
+
+
+
+// console.log(((3 * a) / c) + a + b + c);
+
+
+
+// console.log(a - (b + c) * a / 1);
+
+// /*for(var i = 0; i <= 50; i++ ) {
+//     console.log(i);
+// } */
+
+// for(var i = 0; i <= 50; i++) {
+//     if( i % 2 !== 0 ) {
+//         console.log( i );
+//     }
+// }
+ 
+// var residuo = 10 % 2;
+// console.log( residuo ); // 0 | 10 es par
+
+// var residuo = 7 % 2;
+// console.log( residuo ); // 1 | 7 es impar
+
+const factorial = function fac(n) { return n < 2 ? 1 : n * fac(n - 1) }
+
+console.log(factorial(3))
+
+
+ 
